b7d83a1f8f13563643e1cefe7d695f40288ed237
Tim Mendoza <timmydoza@gmail.com>
2016-04-28 01:59:32 -0700

    add parallax background image

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,16 @@
 var overlay = document.getElementsByClassName('overlay')[0];
 var underlay = document.getElementsByClassName('underlay')[0];
-var headerHeight = 100; //pixels
+var img = document.getElementsByClassName('image-wrapper')[0];
+var headerMax = 400; //pixels
+var headerMin = 50;
+var headerHeight = 100;
+var ratio = headerMax / headerMin;
 
 window.onscroll = function() {
-  var offset = (window.scrollY > headerHeight) ? headerHeight / 2 : window.scrollY / 2;
-  underlay.style.height = headerHeight - offset + 'px'; //implicit type coersion is happening here
-  underlay.style.opacity = (window.scrollY > headerHeight) ? 0.8 : window.scrollY / (headerHeight / 0.8);
-  overlay.style.top = (window.scrollY > headerHeight) ?  headerHeight / 12 + 'px' : headerHeight / 3 - window.scrollY / 4 + 'px';
+  var scroll = window.scrollY;
+  var offset = (scroll > headerMax) ? headerMin : scroll / ratio;
+  underlay.style.height = 100 - offset + 'px'; //implicit type coersion is happening here
+  underlay.style.opacity = (scroll > headerMax) ? 0.8 : scroll / (headerMax / 0.8);
+  overlay.style.top = (scroll > headerMax) ?  headerHeight / 12 + 'px' : headerHeight / 3 - scroll / (ratio * 2) + 'px';
+  img.style.backgroundPosition = '100% ' + (scroll / 3) + 'px';
 };
