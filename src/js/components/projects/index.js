2bf878bf108ef9dc511cedaa9d8b0f69565e0cad
Tim <timmydoza@gmail.com>
2016-09-28 12:43:34 -0700

    fix alignment

--- src/js/components/projects/index.js
+++ src/js/components/projects/index.js
@@ -1,12 +1,13 @@
 var React = require('react');
 var Project = require('./project');
+var styles = require('./style.scss');
 
 var projects = require('./project-data.js');
 
 module.exports = React.createClass({
   render: function() {
     return (
-      <div className="project-wrapper">
+      <div className={styles.projects}>
       {projects.map(function(project) {
         return <Project project={project}/>
       })}
