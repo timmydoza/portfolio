48cdd1b71e2805c3b17e9f72fd94faa00d6b144a
Tim Mendoza <timmydoza@gmail.com>
2016-09-28 21:25:16 -0400

    add zoom effect to background

--- src/js/components/background/index.js
+++ src/js/components/background/index.js
@@ -4,10 +4,13 @@ var styles = require('./style.scss');
 module.exports = React.createClass({
   render: function() {
     return (
-      <div className={styles.image}>
-        <h1>Software Developer</h1>
-        <div className={styles.line}></div>
-        <h1>Seattle, WA</h1>
+      <div className={styles.background}>
+        <img src="img/Untitled-0.jpg" className={styles.image}/>
+        <div className={styles.text}>
+          <h1>Software Developer</h1>
+          <div className={styles.line}></div>
+          <h1>Seattle, WA</h1>
+        </div>
       </div>
     );
   }
